bfe25517db07d1ef25bf55ca7441f0f9bc346dce
Rohit-Pal-21 <kbtug20261@kbtcoe.org>
2022-02-20 22:41:03 +0530

    final commit

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-iv$b&6v#c58*j-uf$z#b6g&z$c@6gi9pre)$ete-&-!$&3#51*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','https://studentstudyportal.azurewebsites.net']
 
 
 # Application definition
