50bae29a1fb2699640b554aec78e9b4ce45a7eef
Rohit-Pal-21 <kbtug20261@kbtcoe.org>
2022-02-20 20:47:25 +0530

    final commit

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,22 +2,22 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns =[ 
-    path('https://studentstudyportal.azurewebsites.net',views.home,name="home"),
-    path('https://studentstudyportal.azurewebsites.net/notes',views.notes,name="notes"),
-    path('https://studentstudyportal.azurewebsites.net/delete_note/<int:pk>',views.delete_note,name="delete-note"),
-    path('https://studentstudyportal.azurewebsites.netnotes_detail/<int:pk>',views.NotesDetailView.as_view(),name="notes-detail"),
-    path('https://studentstudyportal.azurewebsites.net/homework',views.homework,name="homework"),
-    path('https://studentstudyportal.azurewebsites.net/update_homework/<int:pk>',views.update_homework,name="update-homework"),
-    path('https://studentstudyportal.azurewebsites.net/delete_homework/<int:pk>',views.delete_homework,name="delete-homework"),
-    path('https://studentstudyportal.azurewebsites.net/youtube',views.youtube,name="youtube"),
-    path('https://studentstudyportal.azurewebsites.net/todo',views.todo,name="todo"),
-    path('https://studentstudyportal.azurewebsites.net/update_todo/<int:pk>',views.update_todo,name="update-todo"),
-    path('https://studentstudyportal.azurewebsites.net/delete_todo/<int:pk>',views.delete_todo,name="delete-todo"),
-    path('https://studentstudyportal.azurewebsites.net/books',views.books,name="books"),
-    path('https://studentstudyportal.azurewebsites.net/books',views.books,name="books"),
-    path('https://studentstudyportal.azurewebsites.net/dictionary',views.dictionary,name="dictionary"),
-    path('https://studentstudyportal.azurewebsites.net/wiki',views.wiki,name="wiki"),
-    path('https://studentstudyportal.azurewebsites.net/conversion',views.conversion,name="conversion")
+    path('',views.home,name="home"),
+    path('/notes',views.notes,name="notes"),
+    path('/delete_note/<int:pk>',views.delete_note,name="delete-note"),
+    path('/notes_detail/<int:pk>',views.NotesDetailView.as_view(),name="notes-detail"),
+    path('/homework',views.homework,name="homework"),
+    path('/update_homework/<int:pk>',views.update_homework,name="update-homework"),
+    path('/delete_homework/<int:pk>',views.delete_homework,name="delete-homework"),
+    path('/youtube',views.youtube,name="youtube"),
+    path('/todo',views.todo,name="todo"),
+    path('/update_todo/<int:pk>',views.update_todo,name="update-todo"),
+    path('/delete_todo/<int:pk>',views.delete_todo,name="delete-todo"),
+    path('/books',views.books,name="books"),
+    path('/books',views.books,name="books"),
+    path('/dictionary',views.dictionary,name="dictionary"),
+    path('/wiki',views.wiki,name="wiki"),
+    path('/conversion',views.conversion,name="conversion")
 
 
 
